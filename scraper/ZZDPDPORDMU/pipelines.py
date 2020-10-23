058d752849366e723c83cee73dcf447c1ed10c9d
nikola <nikola.tulechki@ontotext.com>
2020-10-23 15:02:40 +0300

    change output filenames

--- scraper/ZZDPDPORDMU/pipelines.py
+++ scraper/ZZDPDPORDMU/pipelines.py
@@ -13,8 +13,8 @@ from ZZDPDPORDMU.settings import SCRAPED_DATA_LOCATION
 
 class DeclarationsRegisterPipeline:
     def process_item(self, item, spider):
-        if len(item['contracts']) > 0:
-            file_name = '%s (ЕИК %s).json' % (item['legal_entity_name'], item['legal_entity_id'])
+        if len(item['contracts']) >= 0:
+            file_name = '%s.json' % (item['legal_entity_id'])
             file_path = os.path.join(SCRAPED_DATA_LOCATION, file_name)
             with open(file_path, 'w', encoding='utf-8') as fp:
                 json.dump(item, fp, ensure_ascii=False, indent=4, separators=(',', ': '))
